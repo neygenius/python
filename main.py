44d163f81a2a163e1a58d585ef365ef86edeac6e
neygenius <117530585+neygenius@users.noreply.github.com>
2022-11-06 19:39:30 +0400

    add main.py
    I wrote a function that finds and records the url of the required number of images for a search query

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
+def get_images_urls (url, keyword):
+    page = 1
+    url_list = []
+    while True:
+        url_page = f"{url}search?p={page}&text={keyword}"
+        headers = {"User-Agent":"Mozilla/5.0"}
+        html_page = requests.get(url_page, headers=headers)
+        soup = BeautifulSoup(html_page.content, "html.pasrer")
+        for img in soup.find_all("img"):
+            img_url = img.attrs.get("src")
+            if not img_url:
+                continue
+            img_url = urljoin(url_page, img_url)
+        page += 1
+        if len(url_list) > 1500:
+            break
+    return url_list
+
+
+
+
