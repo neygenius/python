ed3aca87f961859249311caeec81395c60205e64
neygenius <117530585+neygenius@users.noreply.github.com>
2022-11-13 03:44:47 +0400

    fixed the bugs

--- main.py
+++ main.py
@@ -3,58 +3,89 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse
 
-def is_valid(url):
-    parsed = urlparse(url)
-    return bool(parsed.netloc) and bool(parsed.scheme)
 
-def get_images_urls (url, keyword):
+URL = "https://yandex.ru/images/"
+HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"}
+
+
+def is_valid(url_page: str, img_url: str) -> str:
+    if img_url:
+        parsed = urlparse(img_url)
+        if bool(parsed.netloc) and bool(parsed.scheme):
+            img_url = urljoin(url_page, img_url)
+            return img_url
+
+
+def get_images_urls(url: str, keyword: str, headers: dict, count=1000) -> list:
+    '''
+    Функция собирает список url ведущих к файлам изображений со страницы с поисковым запросом
+    '''
     page = 1
     url_list = []
     while True:
         url_page = f"{url}search?p={page}&text={keyword}"
-        html_page = requests.get(url_page, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"})
+        html_page = requests.get(url_page, headers=headers)
         soup = BeautifulSoup(html_page.content, "html.parser")
         for img in soup.find_all("img"):
             img_url = img.attrs.get("src")
-            if not img_url:
-                continue
-            img_url = urljoin(url_page, img_url)
-            if is_valid(img_url):
+            if is_valid(url_page, img_url):
                 url_list.append(img_url)
+            else:
+                continue
         page += 1
-        if len(url_list) > 1000:
+        if len(url_list) > count:
             break
     return url_list
 
-def download_one_image (url, path, num):
-    if not os.path.isdir(path):
-        os.mkdir(path)
-    img = requests.get(url)
-    file = f"{path}/{str(num).zfill(4)}.jpg"
-    save = open(file, "wb")
-    save.write(img.content)
-    save.close()
 
-def images_count (url, keyword):
-    imgs = get_images_urls(url, keyword)
+def download_one_image(url: str, path: str, num: int) -> None:
+    '''
+    Функция скачивает и сохраняет одно изображение
+    '''
+    if not os.path.isdir(path):
+        try:
+            os.mkdir(path)
+        except PermissionError:
+            print("Insufficient permissions to create a directory on the specified path")
+    img = requests.get(url)
+    file_name = f"{str(num).zfill(4)}.jpg"
+    file = os.path.join(path, file_name)
+    with open(file, "wb") as save:
+        save.write(img.content)
+        save.close()
+
+
+def accounting_for_downloads(url: str, keyword: str, headers: dict) -> int:
+    '''
+    Функция загружает и подсчитывает количество загруженных изображений по указанному поискового запроса
+    '''
+    imgs = get_images_urls(url, keyword, headers)
     num = 0
     for img in imgs:
         download_one_image(img, keyword, num)
         num += 1
     return num
 
-def image_download (url, path, keywords):
+
+def image_download(url: str, path: str, keywords: list, headers: dict) -> None:
+    '''
+    Функция вызывающая остальные функции и оповещающая о том, по какому запросу произодится парсинг
+    и сколько изображений было скачано по итогу
+    '''
     os.chdir(path)
     if not os.path.isdir("dataset"):
-        os.mkdir("dataset")
+        try:
+            os.mkdir("dataset")
+        except PermissionError:
+            print("Insufficient permissions to create a directory on the specified path")
     os.chdir("dataset")
     for i in range(len(keywords)):
         print(keywords[i])
-        amount = images_count(url, keywords[i])
+        amount = accounting_for_downloads(url, keywords[i], headers)
         print(f"{amount} images of a {keywords[i]} have been downloaded")
 
+
 if __name__ == "__main__":
-    url = "https://yandex.ru/images/"
-    path = "C:/Users/mrney/Desktop"
+    path = "C:/Users/user/Desktop"
     keywords = ["polar bear", "brown bear"]
-    image_download(url, path, keywords)
+    image_download(URL, path, keywords, HEADERS)
