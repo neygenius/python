5e7215dfccfbeb7878b92d872e2426b4a42ed295
neygenius <117530585+neygenius@users.noreply.github.com>
2022-12-05 23:43:21 +0400

    another main.py update

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 def is_valid(url: str) -> bool:
     '''
     Функция, проверяющая наличие ссылки на файл в указанном атрибуте (источника)
+    :param url: ссылка на изображение
     '''
     if not url:
         return False
@@ -23,6 +24,10 @@ def is_valid(url: str) -> bool:
 def get_images_urls(url: str, keyword: str, headers: dict, count=1000) -> list:
     '''
     Функция собирает список url ведущих к файлам изображений со страницы с поисковым запросом
+    :param url: ссылка на сайт с изображениями
+    :param keyword: ключевое слово для поиска
+    :param headers: системная информация передаваемая серверу
+    :param count: количество изображений, которые необходимо скачать
     '''
     page = 1
     url_list = []
@@ -47,17 +52,19 @@ def get_images_urls(url: str, keyword: str, headers: dict, count=1000) -> list:
 def download_one_image(url: str, path: str, num: int) -> None:
     '''
     Функция скачивает и сохраняет одно изображение
+    :param url: ссылка на изображение
+    :param path: путь к папке, куда загружаются изображения 
+    :param num: номер изображения
     '''
     path = os.path.join("dataset", path)
     if not os.path.isdir(path):
         try:
             os.mkdir(path)
-        except PermissionError:
-            print("Insufficient permissions to create a directory on the specified path")
-            print("Error: ", sys.exc_info()[0])
+        except PermissionError as ex:
+            print(f"Exception happened: {ex}")
+            sys.exit()
     img = requests.get(url)
-    file_name = f"{str(num).zfill(4)}.jpg"
-    file = os.path.join(path, file_name)
+    file = os.path.join(path, f"{str(num).zfill(4)}.jpg")
     with open(file, "wb") as save:
         save.write(img.content)
 
@@ -65,6 +72,9 @@ def download_one_image(url: str, path: str, num: int) -> None:
 def accounting_for_downloads(url: str, keyword: str, headers: dict) -> int:
     '''
     Функция загружает и подсчитывает количество загруженных изображений по указанному поискового запроса
+    :param url: ссылка на изображение
+    :param keyword: ключевое слово для поиска
+    :param headers: системная информация передаваемая серверу
     '''
     imgs = get_images_urls(url, keyword, headers)
     num = 0
@@ -78,13 +88,16 @@ def image_download(url: str, keywords: list, headers: dict) -> None:
     '''
     Функция вызывающая остальные функции и оповещающая о том, по какому запросу произодится парсинг
     и сколько изображений было скачано по итогу
+    :param url: ссылка на сайт с изображениями
+    :param keywords: список ключевых слов
+    :param headers: системная информация передаваемая серверу
     '''
     if not os.path.isdir("dataset"):
         try:
             os.mkdir("dataset")
-        except PermissionError:
-            print("Insufficient permissions to create a directory on the specified path")
-            print("Error: ", sys.exc_info()[0])
+        except PermissionError as ex:
+            print(f"Exception happened: {ex}")
+            sys.exit()
     for i in range(len(keywords)):
         print(keywords[i])
         amount = accounting_for_downloads(url, keywords[i], headers)
