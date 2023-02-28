ea6b12efdfa64aed4b18c87b64e228ebadfd26a2
neygenius <mr.neygen@yandex.ru>
2023-02-28 23:58:27 +0400

    Добавил возможность выбора другого датасета из меню программы. Добавил проверку на корректность пути. Исправил мелкие недочеты

--- Lab3/main_window.py
+++ Lab3/main_window.py
@@ -2,7 +2,7 @@ import os
 import sys
 from PyQt6 import QtGui
 from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QPushButton, QApplication, QMainWindow, QFileDialog, QLabel
+from PyQt6.QtWidgets import QPushButton, QApplication, QMainWindow, QFileDialog, QLabel, QMessageBox
 from dataset_copy import create_dataset, copy_img
 from main import check_file, KEYWORDS, AnnotationFile
 from random_dataset_copy import random_copy_img, RAND_DICT
@@ -20,18 +20,18 @@ class MainWindow(QMainWindow):
         self.init_ui()
 
     def init_ui(self) -> None:
+        """
+        Инициализация интерфейса
+        """
         self.setWindowTitle("PIC.WORK")
-        self.setMinimumSize(1260, 700)
+        self.setMinimumSize(1300, 725)
         self.setStyleSheet("background-color: #e6e6fa")
 
-        self.folder = QFileDialog.getExistingDirectory(self, "Select folder")
-        src = QLabel(f"MAIN DATASET: {self.folder}", self)
-        src.setStyleSheet("color: #2525ba; font-family: Arial")
-        src.setFixedSize(300, 50)
-        src.move(5, 0)
-
-        self.path_one = self.get_zero_path(KEYWORDS[0])
-        self.path_two = self.get_zero_path(KEYWORDS[1])
+        self.src = QLabel(f"CURRENT DATASET: {self.folder}", self)
+        self.src.setStyleSheet("color: #2525ba; font-family: Arial")
+        self.src.setFixedSize(400, 50)
+        self.src.move(5, 0)
+        self.get_directory()
 
         self.img_one = QLabel(self)
         self.img_one.resize(QSize(600, 400))
@@ -43,25 +43,37 @@ class MainWindow(QMainWindow):
         self.img_two.setStyleSheet("border: None")
         self.img_two.move(640, 200)
 
-        button_create_annotation = self.add_button("CREATE AN ANNOTATION", 25, 80)
+        button_get_directory = self.add_button("SELECT A NEW DIRECTORY", 20, 80)
+        button_get_directory.clicked.connect(self.get_directory)
+
+        button_create_annotation = self.add_button("CREATE AN ANNOTATION", 200, 80)
         button_create_annotation.clicked.connect(self.create_annotation)
 
-        button_copy = self.add_button("COPY A DATASET", 225, 80)
+        button_copy = self.add_button("COPY A DATASET", 380, 80)
         button_copy.clicked.connect(self.dataset_copy)
 
-        button_random_copy = self.add_button("RANDOM COPY A DATASET", 425, 80)
+        button_random_copy = self.add_button("RANDOM COPY A DATASET", 560, 80)
         button_random_copy.clicked.connect(self.random_copy)
 
-        next_p_bear_button = self.add_button("SHOW NEXT POLAR BEAR", 625, 80)
+        next_p_bear_button = self.add_button("SHOW NEXT POLAR BEAR", 740, 80)
         next_p_bear_button.clicked.connect(lambda class_name=KEYWORDS[0]: self.next_image(KEYWORDS[0]))
 
-        next_b_bear_button = self.add_button("SHOW NEXT BROWN BEAR", 825, 80)
+        next_b_bear_button = self.add_button("SHOW NEXT BROWN BEAR", 920, 80)
         next_b_bear_button.clicked.connect(lambda class_name=KEYWORDS[1]: self.next_image(KEYWORDS[1]))
 
-        exit_button = self.add_button("QUIT", 1025, 80)
+        exit_button = self.add_button("QUIT", 1100, 80)
         exit_button.clicked.connect(self.quit)
 
         self.show()
+
+    def get_directory(self) -> None:
+        self.folder = QFileDialog.getExistingDirectory(self, "Select folder")
+        if os.path.isdir(os.path.join(self.folder, KEYWORDS[0])) & os.path.isdir(os.path.join(self.folder, KEYWORDS[1])):
+            self.path_one = self.get_zero_path(KEYWORDS[0])
+            self.path_two = self.get_zero_path(KEYWORDS[1])
+            self.src.setText(f"CURRENT DATASET: {self.folder}")
+        else:
+            QMessageBox.about(self, "Error", f"The selected directory does not contain folders: {KEYWORDS[0]}, {KEYWORDS[1]}")
 
     def get_zero_path(self, keyword: str) -> str:
         """
@@ -92,7 +104,6 @@ class MainWindow(QMainWindow):
         Создание аннотации
         """
         check_file(self.folder)
-        n = 0
         for keyword in KEYWORDS:
             obj = AnnotationFile(self.folder, keyword)
             for num in range(len(os.listdir(os.path.join(self.folder, keyword)))):
@@ -103,6 +114,9 @@ class MainWindow(QMainWindow):
         Копирование датасета
         """
         new_folder = QFileDialog.getExistingDirectory(self, "Select folder for copying")
+        if not new_folder:
+            QMessageBox.about(self, "Error", f"Directory not selected")
+            return
         if new_folder != self.folder:
             create_dataset(new_folder)
             check_file(new_folder)
@@ -110,11 +124,15 @@ class MainWindow(QMainWindow):
                 for num in range(len(os.listdir(os.path.join(self.folder, keyword)))):
                     copy_img(AnnotationFile(new_folder, keyword), self.folder, num)
 
+
     def random_copy(self) -> None:
         """
         Копирует датасет в другую директорию с присвоением случайного номера
         """
         new_r_folder = QFileDialog.getExistingDirectory(self, "Select folder for copying")
+        if not new_r_folder:
+            QMessageBox.about(self, "Error", f"Directory not selected")
+            return
         if new_r_folder != self.folder:
             create_dataset(new_r_folder)
             check_file(new_r_folder)
@@ -145,6 +163,9 @@ class MainWindow(QMainWindow):
                                                                            aspectRatioMode=Qt.AspectRatioMode.KeepAspectRatio))
 
     def quit(self) -> None:
+        """
+        Закрывает окно программы (завершает ее)
+        """
         self.close()
 
 
@@ -152,8 +173,3 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
     app.exec()
-
-
-
-
-
